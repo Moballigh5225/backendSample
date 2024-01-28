67b4fcffa3bcab80053731f035efecc13a09fa26
Moballigh5225 <moballigh5225@gmail.com>
2024-01-28 15:44:04 +0530

    new code added

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("hello server is on");
 });
 
+app.get("/login", (req, res) => {
+  res.send("<h1>login page is here</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Sample Server is running on ${port}`);
 });
